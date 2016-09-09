ba0e06b62920642c57704df5c2ed5442dc4e5f8f
Suman Debnath <sd887851@avagotech.net>
2016-09-09 14:09:34 +0530

    Adding Examples

---
+++ PythonExamples/Level101/examples/02-fizz-buzz-modified.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+
+"""
+Write a program to print fizz, buzz or fizzbuzz if one of the conditions is meet.
+
+If the number is a multiple of 3, print fizz; a multiple of 5, print buzz;
+and if a multiple of both 3 and 5, print fizzbuzz.
+"""
+
+# Below is function definition
+def main():
+    # Create variable named num.
+    # HINT:
+    # if cond:
+    #     pass
+    # elif cond:
+    #     pass
+    # else:
+    #     pass
+    pass
+
+
+# __name__ holds the name of the current module
+if __name__ == "__main__":
+    main() # call main function
---
+++ PythonExamples/README.md
@@ -0,0 +1,6 @@
+pythonbyexamplestutorial
+========================
+
+Set of examples problems to learn python
+
+The repo contains materials of Bangpypers meetup workshop materials.
